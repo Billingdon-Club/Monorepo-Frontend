2acab9513fb8a8282840c9df81c65c527dc9eac9
Joshua Laurence <laurence11@fb.com>
2023-02-10 17:37:49 +0000

    Starting building out front end for a bit of fun :)

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -2,8 +2,22 @@ import "./style.css";
 import Navbar from "../../components/Navbar";
 export default function Splash(props) {
 	return (
-		<div className='splashMain'>
+		<div
+			className='splashMain'
+			onMouseMove={(e) => {
+				document.getElementById("cursorRadialGradient").style.top = `${
+					e.pageY - 1000
+				}px`;
+				document.getElementById("cursorRadialGradient").style.left = `${
+					e.pageX - 1000
+				}px`;
+			}}>
 			<Navbar />
+
+			<div className='cursorRadialGradient' id='cursorRadialGradient'></div>
+			<div className='splashContent'>
+				<h1 className='splashTitle'>monorepo</h1>
+			</div>
 		</div>
 	);
 }
