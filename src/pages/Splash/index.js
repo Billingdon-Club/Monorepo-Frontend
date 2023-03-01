e3e32a9bac4a47621ce1070da9392d30e5d850ea
Alan Parmenter <116365548+alanparmeta@users.noreply.github.com>
2023-03-01 11:50:56 +0000

    Revamp splash page

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -2,21 +2,27 @@ import "./style.css";
 import Navbar from "../../components/Navbar";
 export default function Splash(props) {
 	return (
-		<div
-			className='splashMain'
-			onMouseMove={(e) => {
-				document.getElementById("cursorRadialGradient").style.top = `${
-					e.pageY - 1500
-				}px`;
-				document.getElementById("cursorRadialGradient").style.left = `${
-					e.pageX - 1500
-				}px`;
-			}}>
-			<Navbar currentPage='splash' />
-
-			<div className='cursorRadialGradient' id='cursorRadialGradient'></div>
+		<div>
+			<Navbar currentPage='splash'/>
 			<div className='splashContent'>
-				<h1 className='splashTitle'>monorepo</h1>
+				<div className='splashBody'>
+					<br />
+					Welcome to Monorepo!<br />
+					<br />
+					We are navigating around with keyboards and pointing devices! <br />
+					<br />
+					You can copy and paste as usual: paste will create a new snippet, and copy
+					can be captured from any selection of the content or you can use the round clipboard
+					button on the right to copy the whole snippet. <br />
+					<br />
+					Delete also has its own round button on the left, should you wish to remove
+					a snippet. <br />
+					<br />
+					Languages are automatically detected, although you may need to use the
+					dropdown menu to select manually, above the snippet on the left hand side. <br />
+					<br />
+					To get started, register or log in from the navbar at the top of the page; you will also have a profile associated with your account, which you can always access from the icon in the top right corner. <br />
+				</div>
 			</div>
 		</div>
 	);
